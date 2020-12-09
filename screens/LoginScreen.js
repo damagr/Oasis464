8b78c51e2a1e56f3d834f5edafa4cac0aa88e4a7
damagr <damiamartinez94@gmail.com>
2020-12-09 17:35:32 +0100

    Eliminadas los ficheros residuales de la plantilla.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,7 +3,6 @@
 import React, {Component} from 'react';
 import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
 import {Logo} from '../components/Logo';
-import {Input} from 'react-native-elements';
 
 const style = require('../components/Styles');
 
@@ -15,14 +14,14 @@ export default class LoginScreen extends Component {
 
                     <Logo style={style.logo}/>
                     <View style={{flex: 1, justifyContent: 'center'}}>
-                        <TouchableOpacity style={style.buttonFees}>
+                        <TouchableOpacity style={style.buttonLogin}>
                             <Text style={style.textButtons}>Crear Filtros</Text>
                         </TouchableOpacity>
                     </View>
                     <View style={{flex: 1, justifyContent: 'center'}}>
-                    <TouchableOpacity style={style.buttonFees}>
-                        <Text style={style.textButtons}>Crear Batallas</Text>
-                    </TouchableOpacity>
+                        <TouchableOpacity style={style.buttonLogin}>
+                            <Text style={style.textButtons}>Crear Batallas</Text>
+                        </TouchableOpacity>
                     </View>
                 </ScrollView>
 
