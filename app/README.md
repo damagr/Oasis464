49df3c4f0cdcd8d6a8ae6313c22f7ddd2bc42aeb
damagr <damiamartinez94@gmail.com>
2021-02-17 18:41:02 +0100

    Creación de nuevo proyecto el anterior da problemas

--- app/README.md
+++
@@ -1,2 +0,0 @@
-# Oasis464 - Ultimate Battle
-Aplicación para la asociación Oasis464. Crear y randomizar batallas de gallos (batallas de rap).
---
+++ oasis/screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { OButton } from '../components/OButton';
+import { style } from '../components/OConstants';
+import { Logo } from '../components/OLogo';
+
+export default class LoginScreen extends Component {
+    render() {
+        return (
+            <View style={style.container}>
+                <Logo style={style.logo} />
+                <OButton text={'Admin'}></OButton>
+                <OButton text={'Invitado'}></OButton>
+            </View>
+        );
+    }
+}
